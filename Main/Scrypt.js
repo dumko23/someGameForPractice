3f7e309a2dc19fd917c55eab16dbfa99dd12e7d7
Dima <80668534+dumko23@users.noreply.github.com>
2021-05-05 13:44:48 +0300

    Add files via upload

--- Main/Scrypt.js
+++ Main/Scrypt.js
@@ -1,7 +1,9 @@
 //здесь инициируем героев
-function DueList (name, health, attackMax, attackMin, missChance, critChance, critMod) {
+function DueList (image, name, health, healthInit, attackMax, attackMin, missChance, critChance, critMod) {
+    this.image = image;
     this.name = name;
     this.health = health;
+    this.healthInitial = healthInit;
     this.attackMax = attackMax;
     this.attackMin = attackMin;
     this.missChance = missChance;
@@ -23,148 +25,77 @@ function DueList (name, health, attackMax, attackMin, missChance, critChance, cr
     this.wasChosen = false;
     this.winCount = 0;
 }
-let player1 = new DueList("Clark", 55,2,1,13,17,1.5),
-    player2 = new DueList("John", 45, 3, 2, 19, 22,2),
-    player3 = new DueList("Irene", 40, 3, 3, 27, 18,3),
-    player4 = new DueList("Bork", 35, 5, 4, 34, 30, 4),
-    player5 = new DueList("Joker", 100, 1, 1, 60, 10, 100);
-
-let x,
-    player,
-    enemy;
-let turn,
-    victor,
-    firstRoundStats = [],
-    exitGame = false,
-    damage;
-let round = 1;
+let Clark = new DueList('Clark.png', "Clark", 55, 55,2,1,13,17,1.5),
+    John = new DueList('John.png', "John", 45, 45,3, 2, 19, 22,2),
+    Irene = new DueList('Irene.png', "Irene", 40, 40,3, 3, 27, 18,3),
+    Bart = new DueList('Bart.png', "Bart", 35, 35, 5, 4, 34, 30, 4),
+    Joker = new DueList('Joker.png', "Joker", 100, 100, 1, 0, 60, 10, 100);
 
 
-
-
-//здесь выбираем героев и первого противника
-function chooseClark() {
-    player = player1;
-    player.activePlayer = true;
-    enemyInitiation();
-    document.getElementById('selectHero').hidden = true;
-    document.getElementById('battleDiv').hidden = false;
-    document.getElementById('log').hidden = false;
-    announcement();
-    if (player1.health <= 0){
-        document.getElementById('clark').hidden = true;
-    }
+let heroPickList = [Clark, John, Irene, Bart, Joker];
+function random0N (N){
+    return Math.floor(Math.random() * N);
 }
-function chooseJohn() {
-    player = player2;
-    player.activePlayer = true;
-    enemyInitiation();
-    document.getElementById('selectHero').hidden = true;
-    document.getElementById('battleDiv').hidden = false;
-    document.getElementById('log').hidden = false;
-    announcement();
-    if (player2.health <= 0){
-        document.getElementById('john').hidden = true;
-    }
-}
-function chooseIren() {
-    player = player3;
-    player.activePlayer = true;
-    enemyInitiation();
-    document.getElementById('selectHero').hidden = true;
-    document.getElementById('battleDiv').hidden = false;
-    document.getElementById('log').hidden = false;
-    announcement();
-    if (player3.health <= 0){
-        document.getElementById('iren').hidden = true;
-    }
-}
-function chooseBork() {
-    player = player4;
-    player.activePlayer = true;
-    enemyInitiation();
-    document.getElementById('selectHero').hidden = true;
-    document.getElementById('battleDiv').hidden = false;
-    document.getElementById('log').hidden = false;
-    announcement();
-    if (player4.health <= 0){
-        document.getElementById('bork').hidden = true;
-    }
-
-}
-function chooseJoker() {
-    player = player5;
-    player.activePlayer = true;
-    enemyInitiation();
-    document.getElementById('selectHero').hidden = true;
-    document.getElementById('battleDiv').hidden = false;
-    document.getElementById('log').hidden = false;
-    announcement();
-    if (player5.health <= 0){
-        document.getElementById('joker').hidden = true;
-    }
-}
-
-/*function whatsYourHero(name, hero, id){
-    name = hero;
-    name.activePlayer = true;
-    console.log(player);
-    enemyInitiation();
-    document.getElementById('selectHero').hidden = true;
-    document.getElementById('battleDiv').hidden = false;
-    document.getElementById('log').hidden = false;
-    announcement();
-    if (hero.health <= 0){
-        document.getElementById(id).hidden = true;
-    }
-}*/
-
-function enemyInitiation() {
+let enemy;
+function enemyPick(){
     do {
-        x = chooseEnemy();
-        selectEnemy();
-    } while (enemy.activePlayer === true  || enemy.wasChosen === true);
+        enemy = heroPickList[random0N(heroPickList.length)];
+    } while (enemy === player);
+    return enemy;
+}
+
+function heroPick(hero){
+    return player = hero;
 }
 
 
+function onHeroClick(hero, nameId, healthId, hLeftId, damageId, imageId) {
 
-//выбор противника
-function chooseEnemy() {
-    return Math.floor(Math.random() * (5 - 1 + 1) + 1);
+    heroList(hero, nameId, healthId, hLeftId, damageId, imageId);
+    document.getElementById('heroStats').style.display = "block";
+    document.getElementById('pickButton').hidden = false;
 }
-function selectEnemy() {
-    switch(x) {
-        case 1:
-            enemy = player1;
-            break;
-        case 2:
-            enemy = player2;
-            break;
-        case 3:
-            enemy = player3;
-            break;
-        case 4:
-            enemy = player4;
-            break;
-        case 5:
-            enemy = player5;
-            break
+function heroList(hero, nameId, healthId, hLeftId, damageId, imageId){
+    if (document.getElementById(nameId).hasChildNodes()){
+        document.getElementById(nameId).removeChild(document.getElementById(nameId).lastChild);
     }
-    return (enemy);
+    let heroName = document.createElement('span');
+    heroName.textContent = `Имя героя: ${hero.name}`;
+    document.getElementById(nameId).appendChild(heroName);
+
+    if (document.getElementById(healthId).hasChildNodes()){
+        document.getElementById(healthId).removeChild(document.getElementById(healthId).lastChild);
+    }
+    const heroHealth = `Изначальное здоровье: ${hero.healthInitial}`;
+    document.getElementById(healthId).append(heroHealth);
+
+    if (document.getElementById(hLeftId).hasChildNodes()){
+        document.getElementById(hLeftId).removeChild(document.getElementById(hLeftId).lastChild);
+    }
+    let heroHealthLeft = `Текущее здоровье: ${hero.healthInitial}`;
+    document.getElementById(hLeftId).append(heroHealthLeft);
+
+    if (document.getElementById(damageId).hasChildNodes()){
+        document.getElementById(damageId).removeChild(document.getElementById(damageId).lastChild);
+    }
+    let heroDamage = `Урон: ${hero.attackMin} - ${hero.attackMax}`;
+    document.getElementById(damageId).append(heroDamage);
+    addHeroImage(hero, imageId);
 }
 
-function announcement(){
-    document.getElementById("playerLog").innerHTML = player.name;
-    document.getElementById("playerDmg").innerHTML = `${player.attackMin} - ${player.attackMax}`;
-    document.getElementById("playerHlth").innerHTML = player.health;
-    document.getElementById("enemyLog").innerHTML = enemy.name;
-    document.getElementById("enemyDmg").innerHTML = `${enemy.attackMin} - ${enemy.attackMax}`;
-    document.getElementById("enemyHlth").innerHTML = enemy.health;
+function addHeroImage(hero, imageId) {
+    if (document.getElementById(imageId).hasChildNodes()){
+        document.getElementById(imageId).removeChild(document.getElementById(imageId).lastChild);
+    }
+    let heroImage = document.createElement('img');
+    heroImage.src = hero.image;
+    heroImage.style.paddingLeft = "20px";
+    /*heroImage.style.width = "250px";
+    heroImage.style.height = "312px";*/
+    document.getElementById(imageId).appendChild(heroImage);
 }
 
 
-
-//расчет битвы
 function attack(player){
     return (Math.floor(Math.random() * (player.attackMax - player.attackMin + 1)) + player.attackMin);
 }
@@ -181,13 +112,13 @@ function critAttack(dealer, taker) {
 function roundResult(fighter1, fighter2){
     let turnLog = document.createElement('p');
     turnLog.textContent = `Ход № ${turn}.${fighter1.name} наносит удар, причиняя ${damage} урона. У ${fighter2.name} осталось ${fighter2.health} здоровья!\n`;
-    document.getElementById('battleLog').appendChild(turnLog);
+    document.getElementById('log').appendChild(turnLog);
 }
 function roundActions(fighter1, fighter2) {
     if (dice() < fighter1.missChance) {
         let roundLog = document.createElement('p');
         roundLog.textContent = `${fighter1.name} промахнулся!\n `;
-        document.getElementById('battleLog').appendChild(roundLog);
+        document.getElementById('log').appendChild(roundLog);
     } else {
         damage = critCheck(fighter1, fighter2);
         fighter2.health -= damage;
@@ -211,45 +142,28 @@ function fight() {
         enemy.alive = false;
         let victorLog = document.createElement('p');
         victorLog.textContent = `Победитель - ${victor.name} \n `;
-        document.getElementById('battleLog').appendChild(victorLog);
-        roundEnd(player, enemy);
+        document.getElementById('log').appendChild(victorLog);
+        //roundEnd(player, enemy);
+        healthLeft(player, 'pickedHeroHealthLeft');
+        healthLeft(enemy, 'pickedEnemyHealthLeft');
     } else {
         player.health = 0;
         victor = enemy;
         player.alive = false;
         let victorLog = document.createElement('p');
         victorLog.textContent =  `Победитель - ${victor.name} \n `;
-        document.getElementById('battleLog').appendChild(victorLog);
-        roundEnd(enemy, player);
+        document.getElementById('log').appendChild(victorLog);
+        //roundEnd(enemy, player);
+        healthLeft(player, 'pickedHeroHealthLeft');
+        healthLeft(enemy, 'pickedEnemyHealthLeft');
+        document.getElementById('heroLoses').hidden = false;
     }
-
 }
 
-function roundEnd(fighter1, fighter2) {
-    fighter1.exp += 1;
-    fighter1.winCount += 1;
-    fighter2.alive = false;
-    fighter2.wasChosen = true;
-    let victoryCount = document.createElement('p');
-    victoryCount.textContent = `Количество побед - ${fighter1.winCount}`;
-    document.getElementById('battleLog').appendChild(victoryCount);
+function healthLeft(hero, hLeftId){
+    if (document.getElementById(hLeftId).hasChildNodes()){
+        document.getElementById(hLeftId).removeChild(document.getElementById(hLeftId).lastChild);
+    }
+    let heroHealthLeft = `Текущее здоровье: ${hero.health}`;
+    document.getElementById(hLeftId).append(heroHealthLeft);
 }
-
-function announcement1(){
-    console.log(player.name);
-    document.getElementById("playerLog1").innerHTML = player.name;
-    document.getElementById("playerDmg1").innerHTML = `${player.attackMin} - ${player.attackMax}`;
-    document.getElementById("playerHlth1").innerHTML = player.health;
-    document.getElementById("enemyLog1").innerHTML = enemy.name;
-    document.getElementById("enemyDmg1").innerHTML = `${enemy.attackMin} - ${enemy.attackMax}`;
-    document.getElementById("enemyHlth1").innerHTML = enemy.health;
-}
-function statistic(){
-    document.getElementById('winCount').innerHTML = player.winCount;
-    document.getElementById('enemyWinCount').innerHTML = enemy.winCount;
-    document.getElementById('exp').innerHTML = player.exp;
-    document.getElementById('enemyExp').innerHTML = enemy.exp;
-}
-
-
-
